3f9d1428851cca2f9d4c60899ac55d1757d5113c
Atlas_hrp <haoripei@huawei.com>
2022-11-14 16:40:10 +0800

    [断点续训技术项目] 热恢复训练脚本适配

--- samples/research/get_fault_ranks.py
+++ samples/research/get_fault_ranks.py
@@ -1,4 +1,9 @@
 from mindx_elastic.restore_module.fault_rank_manager.fault_rank_manager import FaultRanksDLManager
 
-fault_ranks = FaultRanksDLManager().get_fault_ranks()
-print(fault_ranks)
+try:
+    fault_ranks = FaultRanksDLManager().get_fault_ranks()
+    print(fault_ranks)
+except OSError:
+    exit(1)
+except Exception:
+    exit(1)
