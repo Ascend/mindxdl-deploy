d5d2ba7169b0a5579d1b4403382f655db1d39196
Atlas_hrp <haoripei@huawei.com>
2022-12-02 11:24:35 +0800

    [断点续训技术项目] 热恢复流程：自动恢复业务进程

---
+++ samples/research/insight_analysis/restore_manager/restore_resilience_manager.py
@@ -0,0 +1,285 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import json
+import os
+import warnings
+from ast import literal_eval
+from typing import List, Optional
+
+import mindspore.communication.management as D
+import mindspore.train.serialization as serialization
+
+from constants import constants
+from constants.constants import MAX_RANK_STRING_LENGTH
+from logger.log import srv_log, run_log
+from validator.validators import DirectoryValidator, StringValidator, RankSizeValidator
+
+
+class RestoreManager:
+    """
+    Get configmap resource class
+    """
+
+    def __init__(self):
+        self.__strategy_input_file_path = os.path.realpath(os.getenv(constants.DEFAULT_STRATEGY_INPUT_ENV)) if \
+            os.getenv(constants.DEFAULT_STRATEGY_INPUT_ENV) else None
+
+    @staticmethod
+    def _handle_input_strategy_file(input_strategy_file: str) -> [str, str]:
+        path = os.path.dirname(input_strategy_file)
+        strategy_name = os.path.basename(input_strategy_file)
+        input_strategy_file_processed, _ = os.path.split(path)
+        return input_strategy_file_processed, strategy_name
+
+    @staticmethod
+    def _check_input_parameter_fault_ranks(fault_ranks: str) -> bool:
+        """
+        Check input parameter fault ranks. For rank, its valid value range is [0, 4095]. Any value not in this range
+        will be judged as invalid.
+        :param fault_ranks: unhealthy npu rank id list
+        :return: valid or not
+        """
+        fault_ranks_validation = StringValidator(fault_ranks, max_len=MAX_RANK_STRING_LENGTH)\
+            .check_string_length()\
+            .check()
+        if not fault_ranks_validation.is_valid():
+            return False
+
+        fault_ranks_splits = fault_ranks.split(",")
+        for rank in fault_ranks_splits:
+            if not isinstance(literal_eval(rank), int):
+                return False
+
+            rank_flag = RankSizeValidator(literal_eval(rank))\
+                .check_rank_size_valid()\
+                .check()\
+                .is_valid()
+            if not rank_flag:
+                return False
+        return True
+
+    @staticmethod
+    def _check_path(path: str) -> str:
+        validation = DirectoryValidator(path).check()
+        if not validation.is_valid():
+            return ""
+        return validation.get_value()
+
+    @staticmethod
+    def _init_communication_group() -> bool:
+        """
+        Init commnucation group for multi-card training job.
+        :return: action status
+        """
+        try:
+            D.init()
+        except TypeError:
+            srv_log.error("MindSpore communication init raise TypeError, if backend_name is not a string.")
+            return False
+        except RuntimeError:
+            srv_log.error("MindSpore communication init raise RuntimeError, if device target is invalid, or backend "
+                          "is invalid, or distributed initialization fails, or the environment variables "
+                          "RANK_ID/MINDSPORE_HCCL_CONFIG_PATH have not been exported when backend is HCCL.")
+            return False
+        else:
+            srv_log.info("MindSpore communication init success.")
+            return True
+
+    @staticmethod
+    def _get_communication_group_size() -> int:
+        """
+        Get multi-card training job used communication group size.
+        :return: group size num
+        """
+        try:
+            device_num = D.get_group_size()
+        except TypeError:
+            srv_log.error("MindSpore get group size raise TypeError, check if group is not a string.")
+            return -1
+        except ValueError:
+            srv_log.error("MindSpore get group size raise ValueError, check if backend id invalid.")
+            return -1
+        except RuntimeError:
+            srv_log.error("MindSpore get group size raise RuntimeError, check if HCCL/NCCL is not available.")
+            return -1
+        else:
+            srv_log.info(f"device_num is {device_num}.")
+            return device_num
+
+    @staticmethod
+    def _check_device_num(device_num: int) -> bool:
+        """
+        Check device num is valid or not.
+        :param device_num: number of npu
+        :return: bool, valid or not
+        """
+        device_num_flag = RankSizeValidator(device_num).check_device_num_valid().check().is_valid()
+        if not device_num_flag:
+            return False
+
+        if device_num == 1:
+            run_log.info("Stand alone training job is not supported for strategy recovery.")
+            return False
+        return True
+
+    @staticmethod
+    def _generate_fault_ranks_list(fault_ranks: str) -> List[int]:
+        """
+        Generate fault ranks list according to input fault ranks string.
+        :param fault_ranks: input fault ranks string
+        :return: fault ranks list
+        """
+        fault_ranks_list = []
+        if not fault_ranks:
+            return fault_ranks_list
+
+        fault_ranks_splits = fault_ranks.split(",")
+        for ele in fault_ranks_splits:
+            if StringValidator(ele).can_be_transformed2int().is_valid():
+                fault_ranks_list.append(int(ele))
+        return fault_ranks_list
+
+    @staticmethod
+    def _process_strategy(device_group_info_list: List, fault_ranks_list: List[int] = None) -> [str, str]:
+        """
+        Generate restore ranks string and restore ranks map based on inputs of device group info and fault ranks list.
+        :param device_group_info_list: GROUP_INFO_FILE path list
+        :param fault_ranks_list: fault ranks list
+        :return: restore ranks string and restore ranks map
+        """
+        restore_ranks = set()
+        restore_rank_dict = dict()
+
+        for fault_rank in fault_ranks_list:
+            elements = device_group_info_list[fault_rank]
+            try:
+                if not fault_ranks_list:
+                    elements_for_use = set(elements)
+                else:
+                    elements_for_use = set(elements) - set(fault_ranks_list)
+            except TypeError:
+                restore_ranks = '-1'
+                restore_rank_map_str = ""
+                srv_log.error(f"Get fault ranks for use raise type error, restore ranks is {restore_ranks}, "
+                              f"restore ranks map is {restore_rank_map_str}.")
+                return restore_ranks, restore_rank_map_str
+
+            if not elements_for_use:
+                restore_ranks = '-1'
+                restore_rank_map_str = ""
+                srv_log.info(f"Fault ranks for use is empty, restore ranks is {restore_ranks}, "
+                             f"restore ranks map is {restore_rank_map_str}.")
+                return restore_ranks, restore_rank_map_str
+
+            restore_rank_dict[fault_rank] = sorted(list(elements_for_use))[0]
+            restore_ranks.add(sorted(list(elements_for_use))[0])
+
+        restore_ranks_list = []
+        for idx in restore_ranks:
+            restore_ranks_list.append(str(idx))
+        restore_ranks_str = ",".join(restore_ranks_list)
+        restore_ranks_json = str(json.dumps(restore_rank_dict))
+
+        if not restore_ranks_str or not restore_ranks_json:
+            return "-1", ""
+
+        srv_log.info(f"Restore ranks is {restore_ranks_str}, restore ranks map is {restore_ranks_json}")
+        return restore_ranks_str, restore_ranks_json
+
+    @staticmethod
+    def _get_restore_group_info_list(sub_strategy_input_file_name: str) -> List[int]:
+        """
+        Get parallel model communication information from group info file.
+        :param sub_strategy_input_file_name: group info file
+        :return: group information
+        """
+        try:
+            if not os.path.exists(sub_strategy_input_file_name):
+                return []
+            res = serialization.restore_group_info_list(sub_strategy_input_file_name)
+        except ValueError:
+            srv_log.error("MindSpore restore group info list raise ValueError, check if group information file "
+                          "is correct.")
+            return []
+        except TypeError:
+            srv_log.error("MindSpore restore group info list raise TypeError, check if group_info_file_name is "
+                          "string.")
+            return []
+        else:
+            return res
+
+    def _prepare_device_info(self,
+                             device_num: int,
+                             strategy_input_file_path: str,
+                             strategy_name: str) -> Optional[list]:
+        """
+        Generate distributed deep learning cluster communication info.
+        :param device_num: device number used for distributed deep learning
+        :param strategy_input_file_path: GROUP_INFO_FILE path
+        :param strategy_name: GROUP_INFO_FILE name
+        :return: cluster communication info list
+        """
+        device_group_info_list = []
+        for index in range(device_num):
+            sub_strategy_input_file_path = os.path.join(strategy_input_file_path, f"device{index}")
+            sub_strategy_input_file_name = os.path.join(sub_strategy_input_file_path, strategy_name)
+            res = self._get_restore_group_info_list(sub_strategy_input_file_name)
+            if not res:
+                continue
+
+            if res not in device_group_info_list:
+                device_group_info_list.append(sorted(res))
+        return device_group_info_list
+
+    def generate_restore_strategy(self, fault_ranks: Optional[str]) -> [str, str]:
+        """
+        Generate restore strategy given fault ranks
+        :param fault_ranks: input fault rank string
+        :return: restore rank string and restore rank map
+        """
+        warnings.simplefilter('ignore', ResourceWarning)
+
+        strategy_input_file_path = self._check_path(self.__strategy_input_file_path)
+        if not strategy_input_file_path:
+            return "-1", ""
+
+        strategy_input_file_path, strategy_name = self._handle_input_strategy_file(strategy_input_file_path)
+        if not strategy_input_file_path or not strategy_name:
+            return "-1", ""
+
+        if not self._init_communication_group():
+            return "-1", ""
+
+        device_num = self._get_communication_group_size()
+        if device_num == -1:
+            return "-1", ""
+
+        is_valid_device_num = self._check_device_num(device_num)
+        if not is_valid_device_num:
+            return "-1", ""
+
+        device_group_info_list = self._prepare_device_info(device_num, strategy_input_file_path, strategy_name)
+
+        if not fault_ranks:
+            return self._process_strategy(device_group_info_list)
+
+        fault_ranks_check_flag = self._check_input_parameter_fault_ranks(fault_ranks)
+        if not fault_ranks_check_flag:
+            return "-1", ""
+
+        fault_ranks_list = self._generate_fault_ranks_list(fault_ranks)
+        return self._process_strategy(device_group_info_list, fault_ranks_list)
---
+++ samples/research/insight_analysis/util/fault_ranks_manager.py
@@ -0,0 +1,368 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import collections
+import glob
+import os
+import re
+from abc import ABC, abstractmethod
+from ast import literal_eval
+from typing import Optional, List
+from hashlib import sha256
+
+from constants import constants
+from constants.constants import MAX_FILE_PATH_LENGTH
+from constants.constants import MAX_SIZE
+from constants.constants import MAX_RANK_STRING_LENGTH
+from logger.log import run_log
+from validator.validators import ClassValidator, FileValidator
+from validator.validators import DirectoryValidator
+from validator.validators import MapValidator
+from validator.validators import RankSizeValidator
+from validator.validators import StringValidator
+
+
+class FaultRanksManager(ABC):
+    """
+    Abstract class of fault ranks manager.
+    """
+    @abstractmethod
+    def get_fault_ranks(self, file_path: Optional[str] = None) -> None:
+        raise NotImplementedError
+
+
+class FaultRanksDLManager(FaultRanksManager):
+    """
+    Get fault ranks for fault-torlence distrited training job.
+    For MindX DL situation, fault ranks read from configmap related file
+    For ModelArts sutiation, fault ranks calculated based on checkpoint directory content
+    """
+    def __init__(self):
+        super().__init__()
+        self.fault_config_path = constants.FAULT_RANK_CONFIG
+
+    @staticmethod
+    def _compute_hash(content: str) -> str:
+        try:
+            hash_code = sha256(content.encode('utf-8')).hexdigest()
+            return hash_code
+        except TypeError:
+            return ""
+        except ValueError:
+            return ""
+        except Exception:
+            return ""
+
+    @staticmethod
+    def check_input_file(path: Optional[str]) -> bool:
+        validation = DirectoryValidator(path, max_len=MAX_FILE_PATH_LENGTH) \
+            .check_is_not_none() \
+            .check_dir_name() \
+            .path_should_exist(is_file=True, msg="Cannot find the fault ranks config file.") \
+            .should_not_contains_sensitive_words() \
+            .with_blacklist() \
+            .check()
+
+        if not validation.is_valid():
+            return False
+
+        if not FileValidator(path).check_file_size().check().is_valid():
+            run_log.error("File size exceeds limited size.")
+            return False
+        return True
+
+    def _fault_ranks_process(self, res: Optional[str], fault_ranks: str) -> str:
+        """
+        Get fault ranks process.
+        :param res: configmap content
+        :param fault_ranks: fault rank string
+        :return: processed fault rank string
+        """
+        if not StringValidator(res).is_valid():
+            run_log.error("The content of the fault config file is invalid.")
+            return fault_ranks
+
+        if not MapValidator(literal_eval(res), inclusive_keys=["FaultRankIds"]).is_valid():
+            run_log.error("The content of the fault config file has not key(FaultRankIds)")
+            return fault_ranks
+
+        record_fault_rank_list_content = str(literal_eval(res)["FaultRankIds"])
+        if not StringValidator(record_fault_rank_list_content,
+                               max_len=MAX_RANK_STRING_LENGTH).check_string_length().check().is_valid():
+            run_log.error("The content of fault rank exceed the max string length.")
+            return fault_ranks
+
+        record_fault_rank_list = literal_eval(record_fault_rank_list_content)
+        if not ClassValidator(record_fault_rank_list, collections.Iterable) \
+                .check_isinstance() \
+                .check().is_valid():
+            run_log.error("The value of the key(FaultRankIds) is not list")
+            return fault_ranks
+
+        clear_fault_rank_list = []
+        for ele in record_fault_rank_list:
+            if ele.isdigit() and RankSizeValidator(literal_eval(ele)).check_rank_size_valid().check().is_valid():
+                clear_fault_rank_list.append(ele)
+
+        res_config_map_fault_ranks = ",".join(clear_fault_rank_list)
+        fault_ranks += res_config_map_fault_ranks
+        run_log.info(f"Fault ranks is {fault_ranks}, type is {type(fault_ranks)}")
+        return fault_ranks
+
+    def _restore_ranks_process(self, res: Optional[str], restore_ranks: str) -> str:
+        """
+        Get fault ranks process.
+        :param res: configmap content
+        :param fault_ranks: fault rank string
+        :return: processed fault rank string
+        """
+        if not StringValidator(res).is_valid():
+            run_log.error("The content of the fault config file is invalid.")
+            return restore_ranks
+
+        if not MapValidator(literal_eval(res), inclusive_keys=["RestoreRankIds"]).is_valid():
+            run_log.error("The content of the fault config file has not key(FaultRankIds)")
+            return restore_ranks
+
+        restore_ranks_content = str(literal_eval(res)["RestoreRankIds"])
+
+        if not StringValidator(restore_ranks_content,
+                               max_len=MAX_RANK_STRING_LENGTH).check_string_length().check().is_valid():
+            run_log.error("The content of fault rank exceed the max string length.")
+            return restore_ranks
+
+        record_restore_ranks_list = literal_eval(restore_ranks_content)
+        if not ClassValidator(record_restore_ranks_list, collections.Iterable) \
+                .check_isinstance() \
+                .check().is_valid():
+            run_log.error("The value of the key(FaultRankIds) is not list")
+            return restore_ranks
+
+        clear_restore_ranks_list = []
+        for ele in record_restore_ranks_list:
+            if ele.isdigit() and RankSizeValidator(literal_eval(ele)).check_rank_size_valid().check().is_valid():
+                clear_restore_ranks_list.append(ele)
+
+        res_config_map_restore_ranks = ",".join(clear_restore_ranks_list)
+        restore_ranks += res_config_map_restore_ranks
+        run_log.info(f"Fault ranks is {restore_ranks}, type is {type(restore_ranks)}")
+        return restore_ranks
+
+    def _clear_ecc_ranks(self, res: Optional[str], clear_ecc_ranks: str) -> str:
+        """
+        Get fault ranks process.
+        :param res: configmap content
+        :param fault_ranks: fault rank string
+        :return: processed fault rank string
+        """
+        if not StringValidator(res).is_valid():
+            run_log.error("The content of the fault config file is invalid.")
+            return clear_ecc_ranks
+
+        if not MapValidator(literal_eval(res), inclusive_keys=["ClearEccRankIds"]).is_valid():
+            run_log.error("The content of the fault config file has not key(ClearEccRankIds)")
+            return clear_ecc_ranks
+
+        clear_ecc_ranks_content = str(literal_eval(res)["ClearEccRankIds"])
+
+        if not StringValidator(clear_ecc_ranks_content,
+                               max_len=MAX_RANK_STRING_LENGTH).check_string_length().check().is_valid():
+            run_log.error("The content of clear ecc rank exceed the max string length.")
+            return clear_ecc_ranks
+
+        record_clear_ecc_ranks_list = literal_eval(clear_ecc_ranks_content)
+        if not ClassValidator(record_clear_ecc_ranks_list, collections.Iterable) \
+                .check_isinstance() \
+                .check().is_valid():
+            run_log.error("The value of the key(ClearEccRankIds) is not list")
+            return clear_ecc_ranks
+
+        clear_ecc_ranks_list = []
+        for ele in record_clear_ecc_ranks_list:
+            if ele.isdigit() and RankSizeValidator(literal_eval(ele)).check_rank_size_valid().check().is_valid():
+                clear_ecc_ranks_list.append(ele)
+
+        res_config_map_clear_ecc_ranks = ",".join(clear_ecc_ranks_list)
+        clear_ecc_ranks += res_config_map_clear_ecc_ranks
+        run_log.info(f"Fault ranks is {clear_ecc_ranks}, type is {type(clear_ecc_ranks)}")
+        return clear_ecc_ranks
+
+    def get_fault_ranks(self, ckpt_local_path: Optional[str] = None) -> str:
+        """
+        Get fault ranks in training job when exception happens.
+        :param ckpt_local_path: default is None for DL sutiation
+        :return: fault ranks
+        """
+
+        run_log.info("Start query fault ranks.")
+
+        fault_npu_file_path = os.path.join(self.fault_config_path, constants.FAULT_NPU_FILE)
+        if not self.check_input_file(fault_npu_file_path):
+            run_log.error("Fault npu file path is invalid or check code file path is invalid")
+            return ""
+
+        fault_ranks = ""
+        try:
+            with open(fault_npu_file_path, "r", encoding='utf-8') as fault_config_out:
+                fault_ranks = self._fault_ranks_process(fault_config_out.read(MAX_SIZE), "")
+        except OSError:
+            run_log.error(f"Load fault config file failed, OSError raised")
+        finally:
+            run_log.info("Finish the operation for querying fault ranks")
+
+        return fault_ranks
+
+    def get_restore_ranks(self, ckpt_local_path: Optional[str] = None) -> str:
+        """
+        Get fault ranks in training job when exception happens.
+        :param ckpt_local_path: default is None for DL sutiation
+        :return: fault ranks
+        """
+
+        run_log.info("Start query fault ranks.")
+
+        fault_npu_file_path = os.path.join(self.fault_config_path, constants.FAULT_NPU_FILE)
+        if not self.check_input_file(fault_npu_file_path):
+            run_log.error("Fault npu file path is invalid or check code file path is invalid")
+            return ""
+
+        restore_ranks = ""
+        try:
+            with open(fault_npu_file_path, "r", encoding='utf-8') as fault_config_out:
+                restore_ranks = self._restore_ranks_process(fault_config_out.read(MAX_SIZE), "")
+        except OSError:
+            run_log.error(f"Load fault config file failed, OSError raised")
+        finally:
+            run_log.info("Finish the operation for querying fault ranks")
+
+        return restore_ranks
+
+    def get_clear_ecc_ranks(self, ckpt_local_path: Optional[str] = None) -> str:
+        """
+        Get fault ranks in training job when exception happens.
+        :param ckpt_local_path: default is None for DL sutiation
+        :return: fault ranks
+        """
+        run_log.info("Start query clear ecc ranks.")
+
+        fault_npu_file_path = os.path.join(self.fault_config_path, constants.FAULT_NPU_FILE)
+        if not self.check_input_file(fault_npu_file_path):
+            run_log.error("fault npu file path is invalid or check code file path is invalid")
+            return ""
+
+        ecc_ranks = ""
+        try:
+            with open(fault_npu_file_path, "r", encoding='utf-8') as fault_config_out:
+                ecc_ranks = self._clear_ecc_ranks(fault_config_out.read(MAX_SIZE), "")
+        except OSError:
+            run_log.error(f"Load fault config file failed, OSError raised")
+        finally:
+            run_log.info("Finish the operation for querying fault ranks")
+
+        return ecc_ranks
+
+class FaultRanksMAManager(FaultRanksManager):
+    @staticmethod
+    def _check_path(path: Optional[str]) -> str:
+        validation = DirectoryValidator(path)
+        if not validation.is_valid():
+            return ""
+        return validation.get_value()
+
+    @staticmethod
+    def _get_rank_id(subdir: str) -> str:
+        """
+        Get rank id based on checkpoint sub directory
+        :param subdir: checkpoint sub directory (contains rank information in standard naming principle for
+        distributed model training)
+        :return: rank id
+        """
+        pattern = re.compile(r'\d+')
+        match_res = pattern.findall(subdir)
+        if len(match_res) == 1:
+            string_validation = StringValidator(match_res[0]).can_be_transformed2int().check()
+            rank_id_validation = RankSizeValidator(int(match_res[0])).check_rank_size_valid().check()
+            if string_validation.is_valid() and rank_id_validation.is_valid():
+                return match_res[0]
+        return ""
+
+    def check_input_file(self, ckpt_local_path: Optional[str]) -> str:
+        ckpt_local_path_res = self._check_path(ckpt_local_path)
+        ckpt_local_real_path = os.path.realpath(ckpt_local_path_res)
+        if not DirectoryValidator(ckpt_local_real_path) \
+                .check_is_not_none() \
+                .check_dir_name() \
+                .path_should_exist(is_file=False, msg="Cannot find the checkpoint directory.") \
+                .should_not_contains_sensitive_words() \
+                .check_dir_file_number() \
+                .with_blacklist() \
+                .check() \
+                .is_valid():
+            return ""
+        return ckpt_local_real_path
+
+    def get_fault_ranks(self, ckpt_local_path: Optional[str] = None) -> str:
+        """
+        Inference fault ranks according to saved checkpoint files.
+        :param ckpt_local_path: saved checkpoint files directory
+        :return: fault ranks
+        """
+        run_log.info("Start query fault ranks.")
+        ckpt_local_real_path = self.check_input_file(ckpt_local_path)
+        if not ckpt_local_real_path:
+            return ""
+
+        fault_ranks_list = []
+        try:
+            for dirpath, dirnames, filenames in os.walk(ckpt_local_real_path):
+                self._get_subdir_fault_ranks(dirnames, dirpath, fault_ranks_list)
+        except OSError as os_exp:
+            run_log.error(f"Get fault ranks for modelarts failed, {str(os_exp)}.")
+            return ""
+        except IndexError as index_exp:
+            run_log.error(f"Get fault ranks for modelarts failed, index error raise {index_exp}, "
+                          f"please check checkpoint directory.")
+            return ""
+        except Exception as exp:
+            run_log.error(f"Get fault ranks for modelarts failed, exception raise {exp}.")
+            return ""
+        else:
+            run_log.info("Get fault ranks for modelarts success.")
+            return ",".join(fault_ranks_list)
+
+    def _get_subdir_fault_ranks(self,
+                                dirnames: List[str],
+                                dirpath: str,
+                                fault_ranks_list: List[str]):
+        for subdir in dirnames:
+            candi_dirs = os.path.join(dirpath, subdir)
+            ckpt_pattern = os.path.join(candi_dirs, "*.ckpt")
+            ckpt_exp_pattern = os.path.join(candi_dirs, "*breakpoint.ckpt")
+            ckpt_all_files = glob.glob(ckpt_pattern)
+            ckpt_exp_files = glob.glob(ckpt_exp_pattern)
+
+            if not ckpt_exp_files and not self._get_rank_id(subdir):
+                continue
+
+            if not ckpt_exp_files and self._get_rank_id(subdir):
+                fault_ranks_list.append(self._get_rank_id(subdir))
+                continue
+
+            ckpt_exp_files.sort(key=os.path.getmtime, reverse=True)
+            ckpt_all_files.sort(key=os.path.getmtime, reverse=False)
+            latest_exp_ckpt_file = ckpt_exp_files[0]
+            oldest_ckpt_file = ckpt_all_files[0]
+            if latest_exp_ckpt_file != oldest_ckpt_file and os.path.getsize(latest_exp_ckpt_file) != \
+                    os.path.getsize(oldest_ckpt_file) and self._get_rank_id(subdir):
+                fault_ranks_list.append(self._get_rank_id(subdir))
