d39685759e9eaeef38e1cb7325be2507e1ded3a0
chengjunhua <daisy_chengcjh@163.com>
2023-02-23 22:51:40 +0800

    【修改说明 Modification】去ranktable file 脚本适配

---
+++ samples/train_operator/mindspore/train_start.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# default start shell path
+
+CURPATH="(dirname "$0")"
+. ${CONFIG_PATH}/cache_util.sh
+if [ $# != 3 ] && [ $# != 4] && [ $# != 5 ]
+then
+    echo "Usage: bash train_start.sh [DATASET_PATH] [CONFIG_PATH] [DEVICE_TARGET] [PRETRAINED_CKPT_PATH](optional)"
+	echo "       bash train_start.sh [DATASET_PATH] [CONFIG_PATH] [DEVICE_TARGET] [RUN_EVAL](optional) [EVAL_DATASET_PATH](optional)"
+	exit 1
+fi
+
+get_real_path(){
+  if [ "${1:0:1}" == "/" ]; then
+    echo "$1"
+  else
+    echo "$(realpath -m $PWD/$1)"
+  fi
+}
+
+PATH1=$(get_real_path $1)
+CONFIG_PATH=$(get_real_path $2)
+DEVICE_TARGET=$3
+
+if [ $# == 4 ]
+then 
+    PATH2=$(get_real_path $4)
+fi
+
+if [ $# == 5 ]
+then
+  RUN_EVAL=$4
+  EVAL_DATASET_PATH=$(get_real_path $5)
+fi
+
+if [ ! -d $PATH1 ]
+then
+    echo "error: DATASET_PATH=$PATH1 is not a directory"
+	exit 1
+fi
+
+if [ $# == 4 ] && [ ! -f $PATH2 ]
+then
+    echo "error: PRETRAINED_CKPT_PATH=$PATH2 is not a file"
+	exit 1
+fi
+
+if [ "x${RUN_EVAL}" == "xTrue" ] && [ ! -d $EVAL_DATASET_PATH ]
+then
+    echo "error: EVAL_DATASET_PATH=$EVAL_DATASET_PATH is not a directory"
+	exit 1
+fi
+
+if [ "x{RUN_EVAL}" == "xTrue" ]
+then
+  bootup_cache_server
+  CACHE_SESSION_ID=$(generate_cache_session)
+fi
+
+if [ ${MS_ROLE} == "MS_SCHED"]
+then
+    rm -rf ./sched
+	mkdir ./sched
+	cp ../config/*.yaml ./sched
+	cp ../*.py ./sched
+	cp -r ../src ./sched
+	cd ./sched || exit
+    echo "start scheduler"
+    export DEVICE_ID=0
+	if [ $# == 3 ]
+    then 
+         python train.py --run_distribute=True --device_num=8 --data_path=$PATH1 --parameter_server=False --device_target=$DEVICE_TARGET --config=$CONFIG_PATH --output_path './output'	&> sched.log &
+    fi
+	
+	if [ $# == 4 ]
+	then
+	     python train.py --run_distribute=True --device_num=8 --data_path=$PATH1 --parameter_server=False --device_target=$DEVICE_TARGET --config=$CONFIG_PATH --pre_trained=$PATH2 --output_path './output'	&> sched.log &
+    fi
+fi
+
+if [ ${MS_ROLE} == "MS_WORKER"]
+then
+    echo "start scheduler"
+    for((i=0;i<${MS_LOCAL_WORKER});i++);
+	do
+	   export DEVICE_ID=${i}
+	   rm -rf ./worker_$i
+	   mkdir ./worker_$i
+	   cp ../config/*.yaml ./worker_$i
+	   cp ../*.py ./worker_$i
+	   cp -r ../src ./worker_$i
+	   cd ./worker_$i || exit
+       if [ $# == 3 ]
+	   then 
+           python train.py --run_distribute=True --device_num=8 --data_path=$PATH1 --parameter_server=False --device_target=$DEVICE_TARGET --config=$CONFIG_PATH --output_path './output'	&> sched.log &
+       fi
+	
+	   if [ $# == 4 ]
+	   then
+	       python train.py --run_distribute=True --device_num=8 --data_path=$PATH1 --parameter_server=False --device_target=$DEVICE_TARGET --config=$CONFIG_PATH --pre_trained=$PATH2 --output_path './output'	&> sched.log &
+       fi
+       cd ..
+	done
+fi
+
+
+
---
+++ samples/train_operator/pytorch/train_start.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+# default start shell path
+DLS_USER_HOME_DIR="$(
+  cd "$(dirname "$0")" || exit 1
+  if [[ $? -eq 1 ]]; then
+    exit 1
+  fi
+  pwd -P
+)"
+cd "$DLS_USER_HOME_DIR" || exit 1
+
+# set pythonpath(especially for tensorflow)
+export PYTHONPATH="$DLS_USER_JOB_DIR:$PYTHONPATH"
+export PYTHONUNBUFFERED=1
+
+function dls_get_executor {
+    local filename="$(basename -- "$1")"
+    local extension="${filename##*.}"
+    extension="$(echo "$extension" | tr '[:upper:]' '[:lower:]')"
+    case "$extension" in
+    py|pyc|pyw|pyo|pyd)
+        which python
+        ;;
+    sh)
+        which bash
+        ;;
+    *)
+        ;;
+    esac
+}
+
+function set_env {
+    local install_path=/usr/local/Ascend
+    if [ -d ${install_path}/ascend-toolkit/latest ]; then
+      # use toolkit env
+      source ${install_path}/ascend-toolkit/set_env.sh
+    elif [ -d ${install_path}/nnae/latest ]; then
+      # use nnae env
+      source ${install_path}/nnae/set_env.sh
+    fi
+
+    # use tfplugin env
+    if [ -d ${install_path}/tfplugin/latest ]; then
+      source ${install_path}/tfplugin/set_env.sh
+    fi
+}
+
+function logger {
+    echo "[$(date +%Y%m%d-%H:%M:%S)] [MindXDL Service Log]$*"
+}
+
+echo $@ |grep -q -E '^[ 0-9a-zA-Z,./:_=-]*$'
+ret=$?
+if [ "${ret}" -ne 0 ]; then
+  echo "params error!"
+  exit 1
+fi
+
+# training job input parameters
+code_real_dir=`readlink -f $1`
+if [ -d "${code_real_dir}" ]; then
+    app_url="${code_real_dir}/"
+fi
+log_real_path=`readlink -f $2`
+if [ -f "${log_real_path}" ]; then
+    log_url="${log_real_path}"
+else
+    touch ${log_real_path}
+    log_url="${log_real_path}"
+fi
+boot_file="$3"
+shift 3
+
+function show_help() {
+  echo "Usage train_start.sh /job/code/resnet50 /tmp/log/training.log train.py"
+}
+
+function param_check() {
+  if [ -z "${app_url}" ]; then
+    echo "please input code dir"
+    show_help
+    exit 1
+  fi
+
+  if [ -L ${app_url} ]; then
+    echo "code dir is a link!"
+    exit 1
+  fi
+
+  if [ -z "${boot_file}" ]; then
+    echo "please input boot file"
+    show_help
+    exit 1
+  fi
+
+  if [ -L ${boot_file} ]; then
+    echo "boot file is a link!"
+    exit 1
+  fi
+
+  if [ -z "${log_url}" ]; then
+    echo "please input log url"
+    show_help
+    exit 1
+  fi
+
+  if [ -L ${log_url} ]; then
+    echo "log url is a link!"
+    exit 1
+  fi
+
+}
+
+boot_file_path=${app_url}
+params="$@"
+train_param=${params%%need_freeze*}
+if [[ $@ =~ need_freeze ]]; then
+    freeze_cmd=${params##*need_freeze }
+fi
+
+param_check
+chmod 640 ${log_url}
+
+start_time=$(date +%Y-%m-%d-%H:%M:%S)
+logger "Training start at ${start_time}"
+
+sleep 1
+
+# 获取环境变量中的device_count字段
+device_count=${LOCAL_WORLD_SIZE}
+if [[ "${device_count}" -eq 0 ]]; then
+  echo "device count is 0, train job failed." | tee -a hccl.log
+  chmod 440 ${log_url}
+  exit 1
+fi
+
+# 获取环境变量中的server_count字段
+server_count=·expr ${WORLD_SIZE} / ${LOCAL_WORLD_SIZE}·
+if [[ "${server_count}" == "" ]]; then
+  echo "server count is 0, train job failed." | tee -a hccl.log
+  chmod 440 ${log_url}
+  exit 1
+fi
+
+# 获取device_list
+device_list=${LOCAL_RANK}
+echo "device_list: ${device_list}"
+
+function check_return_code() {
+    if [[ $? -ne 0 ]]; then
+      logger "running job failed." | tee ${log_url}
+      exit 1
+    fi
+}
+
+DLS_PROGRAM_EXECUTOR="$(dls_get_executor "$boot_file")"
+# set training env
+set_env
+export JOB_ID=123456789
+
+# 单节点训练场景
+if [[ "${server_count}" -eq 1 ]]; then
+  server_id=0
+  if [ "${device_count}" -eq 1 ]; then
+    get_env_for_1p_job
+    ${DLS_PROGRAM_EXECUTOR} ${boot_file_path}${boot_file} ${train_param} 2>&1 && tee ${log_url}
+    check_return_code
+    if [[ $@ =~ need_freeze ]]; then
+      ${DLS_PROGRAM_EXECUTOR} ${boot_file_path}${freeze_cmd} 2>&1 && tee ${log_url}
+      check_return_code
+    fi
+    chmod 440 ${log_url}
+    exit 0
+  fi
+fi
+
+# 多节点场景
+if [[ "${server_count}" -ge 1 ]]; then
+  server_id=$(RANK)
+  logger "server id is: ""${server_id}"
+  ${DLS_PROGRAM_EXECUTOR} ${boot_file_path}${boot_file} ${train_param} --multiprocessing-distributed --divice-list=${device_list} --benchmark=0 --device='npu' --addr=${MASTER_ADDR} --world-size=${WORLD_SIZE} --rank=${RANK} && tee ${log_url}
+  check_return_code
+  if [[ $@ =~ need_freeze ]]; then
+    ${DLS_PROGRAM_EXECUTOR} ${boot_file_path}${freeze_cmd} --addr=${MASTER_ADDR} --world-size=${WORLD_SIZE} --rank=${RANK} && tee ${log_url}
+    check_return_code
+  fi
+fi
+
+chmod 440 ${log_url}
