04d7021cc82708f6d6ffa8a8ccb68867fdb8b0e2
Jojo <zhangjunda1@huawei.com>
2023-08-25 07:04:02 +0000

    !311 pytorch支持临终遗言 * pytorch场景支持临终遗言 * pytorch场景支持临终遗言 * pytorch场景支持临终遗言 * pytorch场景支持临终遗言 * pytorch场景支持临终遗言 * pytorch场景支持临终遗言

---
+++ samples/train/resumable-training/fault-rescheduling/withRanktable/pytorch/resnet50/pre_stop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source rank_table.sh
+
+proc_num=`ls /dev/davinci*|grep -v manager | wc -l`
+
+if [[ $LOCAL_WORLD_SIZE ]]; then
+    proc_num=${LOCAL_WORLD_SIZE}
+else
+    # 获取hccl.json中的device_count字段
+    device_count=$(cat "${RANK_TABLE_FILE}" | grep -o device_id | wc -l)
+    if [[ "${device_count}" -eq 0 ]]; then
+      echo "device count is 0, prestop job failed." | tee -a log
+      exit 1
+    fi
+    # 获取hccl.json中的server_count字段
+    server_count=$(get_json_value ${RANK_TABLE_FILE} server_count)
+    if [[ "${server_count}" == "" ]]; then
+      echo "server count is 0, prestop job failed." | tee -a log
+      exit 1
+    fi
+    proc_num=$((device_count / server_count))
+fi
+
+
+PID=$(ps -eaf | grep python | grep -v grep |head -n `expr $proc_num + 1` | tail -n $proc_num| awk '{print $2}')
+
+if [[ "" != "$PID" ]]; then
+  echo "killing $PID" | tee -a log
+  kill -15 $PID
+fi
