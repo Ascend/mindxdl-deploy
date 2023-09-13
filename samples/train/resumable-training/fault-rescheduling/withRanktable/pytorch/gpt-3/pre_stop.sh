e5609feb3c730a2a76001a50824864abf416ab2a
Jojo <zhangjunda1@huawei.com>
2023-09-13 09:39:29 +0800

    修復pt單機單卡臨終遺言保存失敗

--- samples/train/resumable-training/fault-rescheduling/withRanktable/pytorch/gpt-3/pre_stop.sh
+++ samples/train/resumable-training/fault-rescheduling/withRanktable/pytorch/gpt-3/pre_stop.sh
@@ -22,8 +22,11 @@ else
     proc_num=$((device_count / server_count))
 fi
 
-
-PID=$(ps -eaf | grep python | grep -v grep | grep -v semaphore_tracker |head -n `expr $proc_num + 1` | tail -n $proc_num| awk '{print $2}')
+if [[ "${proc_num}" -eq 1 ]]; then
+    PID=$(ps -eaf | grep python | grep -v grep | grep -v semaphore_tracker |head -n 1 | awk '{print $2}')
+else
+    PID=$(ps -eaf | grep python | grep -v grep | grep -v semaphore_tracker |head -n `expr $proc_num + 1` | tail -n $proc_num| awk '{print $2}')
+fi
 
 if [[ "" != "$PID" ]]; then
   echo "killing $PID" | tee log
